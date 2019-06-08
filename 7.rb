95fde1cf44bef1343c3c83725222359825134289
Daniel <dacevedosp@gmail.com>
2019-06-08 10:41:48 -0400

    Finish exercise seven 7.rb

--- 7.rb
+++ 7.rb
@@ -1,19 +1,18 @@
+class Excluye
+    def list(b)
+        File.open('nuevo_catalogo.txt','a'){|v|v.puts("#{b.name},#{b.large},#{b.medium}.#{b.small}")}
+    end
+end
+
 class Product
-    #attr_accessor :
+    attr_accessor :name, :large, :medium, :small
     def initialize(name, *products_list)
         @name = name
         @large = products_list[1].to_i
         @medium = products_list[2].to_i
         @small = products_list[3].to_i
         @xsmall = products_list[4].to_i
-
-        nuevo = File.open('catalago2.txt','a')
-        nuevo = File.write('catalago2.txt',@name)
-
     end
-
-   
-
 end
 
 products_list = []
@@ -24,10 +23,6 @@ data.each do |prod|
     ls = prod.split(', ')
     products_list << Product.new(*ls)
 end
-#print products_list
-# products_list.each do |p|
-#     puts "Producto #{p.name}, promedio = #{p.promedio}" 
-# end
-# products_list.each do |e|
-#     puts e.excluye
-# end
+nuevoc = Excluye.new
+products_list.each {|c|nuevoc.list(c)}
+
