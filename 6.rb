8a0669c005d85366df1475c3daf86c2148138c91
Daniel <dacevedosp@gmail.com>
2019-06-07 21:32:04 -0400

    Finish exercise 6.rb

--- 6.rb
+++ 6.rb
@@ -1,21 +1,38 @@
 class Product
+    #attr_accessor :
     def initialize(*products_list)
         @name = name
-        @large = large
-        @medium = medium
-        @small = small
-        @xsmall = xsmall
+        @large = products_list[1].to_i
+        @medium = products_list[2].to_i
+        @small = products_list[3].to_i
+        @xsmall = products_list[4].to_i
+        @total = @large + @medium + @small + @xsmall
+       # @total = products_list
     end
+
+    def promedio
+        @total = @total / 4 
+    end
+    def name
+        @name 
+    end
+
 end
 
 products_list = []
-data = []
-file = File.open("catalogo.txt","r")
+file = File.open('catalogo.txt','r')
 data = file.readlines
-file.close
+
 data.each do |prod|
     ls = prod.split(', ')
     products_list << Product.new(*ls)
 end
-puts products_list
+#print products_list
+products_list.each do |p|
+    puts "Producto #{p.name}, promedio = #{p.promedio}" 
+end
+
+
+
+
 
